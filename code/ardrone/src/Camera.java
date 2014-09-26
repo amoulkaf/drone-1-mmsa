01279c9b946987fa0a616267441147ce45924551
anasmouagni <anasmouagni@ef62bdd9-8e75-cf03-c011-c9a79ca1a455>
2014-09-26 10:21:23 +0000

    cam

---
+++ code/ardrone/src/Camera.java
@@ -0,0 +1,27 @@
+import java.io.*; 
+import java.net.*;
+
+public class Camera {
+	//Chargement du codec
+	//AT*CONFIG=<SEQ>,"video:video_codec","129"\r;
+	
+	//Camera frontale
+	//AT*CONFIG=<SEQ>,"video:video_channel","0"\r;
+	
+	//Camera verticale
+	//AT*CONFIG=<SEQ>,"video:video_channel","1"\r;
+	
+	//ffplay -flags low_delay tcp://<IP ADDR>:<PORT>
+		
+	public String downloadCodec(int seq,String eof){
+		return "AT*CONFIG="+seq+"\"video:video_codec\",\"129\""+eof;
+	}
+	
+	public String frontCam(int seq,String eof){
+		return "AT*CONFIG="+seq+"\"video:video_channel\",\"0\""+eof;
+	}
+	
+	public String downCam(int seq,String eof){
+		return "AT*CONFIG="+seq+"\"video:video_channel\",\"1\""+eof;
+	}
+}
