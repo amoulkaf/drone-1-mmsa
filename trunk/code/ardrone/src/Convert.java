f4cfb2f2af0d3f20e0f00177a0760597a0369898
lec.geoffrey@gmail.com <lec.geoffrey@gmail.com@ef62bdd9-8e75-cf03-c011-c9a79ca1a455>
2014-09-26 10:09:54 +0000

    Add classe Convert : fonction convert754 et CRC32

---
+++ trunk/code/ardrone/src/Convert.java
@@ -0,0 +1,20 @@
+import java.util.zip.CRC32;
+import java.util.zip.Checksum;
+
+
+public class Convert {
+	//fonction qui permet de convertir un float en int selon la norme IEE754
+	  public static int convert754(double x){
+		  return Float.floatToRawIntBits((float)x);
+	  }
+	  
+	  //fonction qui ,permet de convertir un string ASCII en string CRC32
+	  public static String convertCRC32(String str){
+		  	byte bytes[] = str.getBytes();
+			Checksum checksum = new CRC32();
+			checksum.update(bytes,0,bytes.length);
+			long lngChecksum = checksum.getValue();
+			String ret = Long.toHexString(lngChecksum);
+			return ret;
+	  }
+}
