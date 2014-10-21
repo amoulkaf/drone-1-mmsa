24f5355f2f9e9f60b020221c52bea31adc533a9a
lec.geoffrey@gmail.com <lec.geoffrey@gmail.com@ef62bdd9-8e75-cf03-c011-c9a79ca1a455>
2014-10-21 13:54:26 +0000

    maj min

--- trunk/code/ardrone/src/Main.java
+++ trunk/code/ardrone/src/Main.java
@@ -1,15 +1,47 @@
-import guiView.MainWindowGUI;
+import java.io.*;
 
+import guiView.MainWindowGUI;
 
 public class Main {
 
 	public static void main(String[] args) {
-		new MainWindowGUI();
+		//new MainWindowGUI();
 		
 		Drone ardrone = new Drone("192.168.1.1", 5556, "\r", "AR-Drone");
 		Drone localhost = new Drone("localhost", 7000, "\n", "localhost");
 		  
+		testPy();
+		
 		//ardrone.start();
 		//localhost.start();
 	}
+	
+	public static void testPy(){
+		try{
+			//TEST:GEOFFREY:17-10-2014:test d'import fichier .py
+			String prg ="from PIL.Image import *\n"
+					+ "def main():\n"
+					+ "\timg = open(\"../IHM/ihm.png\")\n"
+					+ "main()";
+
+			System.out.println("1");
+			BufferedWriter out = new BufferedWriter(new FileWriter("test1.py"));
+			out.write(prg);
+			out.close();
+System.out.println("2");
+			ProcessBuilder pb = new ProcessBuilder("python","test1.py");
+			Process p = pb.start();
+			 System.out.println("3");
+			 
+			//BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));
+			
+			//int ret = new Integer(in.readLine()).intValue();
+			
+			//System.out.println("value is : "+ret);
+		}
+		catch(Exception e){System.out.println(e);}
+	}
+	
 }
+
+
