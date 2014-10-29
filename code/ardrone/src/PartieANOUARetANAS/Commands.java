a0d49c9d82825369ae3098385e004e1ca883d0c8
anasmouagni <anasmouagni@ef62bdd9-8e75-cf03-c011-c9a79ca1a455>
2014-10-29 16:52:22 +0000

    Ajout de keyBoard modification des class controller, connection, commands

--- code/ardrone/src/PartieANOUARetANAS/Commands.java
+++ code/ardrone/src/PartieANOUARetANAS/Commands.java
@@ -16,51 +16,94 @@ public final class Commands {
 	  public final static String VALUEVIDEOCHANNELVERTICAL = "1";
 	  public final static String KEYVIDEOCODEC = "video:video_codec";
 	  public final static String VALUEVIDEOCODEC129 ="129";
+	  private static float neg = -1;
+	  private static float pos = 1;
+	  private static long neg754 = Convert.convert754(neg);
+	  private static long pos754 = Convert.convert754(pos);
+	  
+	  //Avancer
+	  public static String forward(int seq){
+		  return move(0,neg754,0,0,seq);
+	  }
+	  
+	  //Reculer
+	  public static String backward(int seq){
+		  return move(0,pos754,0,0,seq);
+	  }
+	  
+	  //Deplacement horizontal a droite
+	  public static String horizontalRight1(int seq){
+		  return move(pos754,0,0,0,seq);
+	  }
+	  
+	  //Deplacement horizontal a gauche
+	  public static String horizontalLeft(int seq){
+		  return move(neg754,0,0,0,seq);
+	  }
+	  
+	  //Decoller
+	  public static String takeOff(int seq){
+		  return "AT*REF=" + seq + "," + TAKEOFF;
+	  } 
+	  
+	  //Atterir
+	  public static String landing(int seq){
+		  return "AT*REF=" + seq + "," + LANDING;
+	  }
+	  
+	  //Pivoter a droite
+	  public static String rotateRight(int seq){
+		  return move(0,0,0,pos754,seq);
+	  }
+	  
+	  //Pivoter a gauche
+	  public static String rotateLeft(int seq){
+		  return move(0,0,0,neg754,seq);
+	  }
+	  
+	  //Descendre
+	  public static String goDown(int seq){
+		  return move(0,0,neg754,0,seq);
+	  }
+	  
+	  //Monter
+	  public static String goUp(int seq){
+		  return move(0,0,pos754,0,seq);
+	  }
 	  
 	  //calibre le drone, une fois stabilise (a faire manuellement)
-	  public static String calibrate(int seq, String ID, String eof){
-		  return "AT*CALIB=" + ++seq + "," + ID +eof;
+	  public static String calibrate(int seq, String ID){
+		  return "AT*CALIB=" + seq + "," + ID ;
 	  }
 	  
 	  //calibrage horizontal : verifie que le drone soit sur un support stable (a faire au debut)
-	  public static String check(int seq, String eof){
-		  return "AT*FTRIM=" + ++seq + eof;
+	  public static String check(int seq){
+		  return "AT*FTRIM=" + seq;
 	  }
 	  
 	  //fait planer le drone (il est stable en l'air)
-	  public static String hover(int seq, String eof){
-		  return "AT*PCMD="+ ++seq + ",0,0,0,0,0" + eof;
+	  public static String hover(int seq){
+		  return "AT*PCMD="+ seq + ",0,0,0,0,0" ;
 	  }
 	  
 	  //fait bouger le drone suivant les angles Roll(L/R), Pitch(F/B), Throttle(Gaz) et Yaw(Angle)
-	  public static String move(int roll, int pitch, int throttle, int yaw, int seq, String eof){
-		  return "AT*PCMD=" + ++seq + "," + ID + "," + roll + "," + pitch + "," + throttle + "," + yaw + eof;
-	  }
-	 
-	  
-	  //fait decoller le drone
-	  public static String takeOff(int seq, String eof){
-		  return "AT*REF=" + ++seq + "," + TAKEOFF + eof;
-	  }
-	  
-	  //pose le drone
-	  public static String landing(int seq, String eof){
-		  return "AT*REF=" + ++seq + "," + LANDING + eof;
+	  public static String move(long roll, long pitch, long throttle, long yaw, int seq){
+		  return "AT*PCMD=" + seq + "," + ID + "," + roll + "," + pitch + "," + throttle + "," + yaw;
 	  }
 	  
 	  //coupe les moteurs du drone (en cas d'urgence uniquement)
-	  public static String emergencyMotorsCut(int seq, String eof){
-		  return "AT*REF="+ ++seq + "," + EMERGENCYMOTORSCUT + eof;
+	  public static String emergencyMotorsCut(int seq){
+		  return "AT*REF="+ seq + "," + EMERGENCYMOTORSCUT;
 	  }
 	  
 	  //commande qui permet de configurer le drone selon la key et sa valeur
-	  public static String config(String key, String value, int seq, String eof){
-		  return "AT*CONFIG=" + ++seq + ",\"" + key + "\",\"" + value + "\"" + eof;
+	  public static String config(String key, String value, int seq){
+		  return "AT*CONFIG=" + seq + ",\"" + key + "\",\"" + value + "\"" ;
 	  }
 	  
 	  //commande a envoyer avant chaque nouvelle config
-	  public static String configIDS(int seq, String eof){
-		  return "AT*CONFIG_IDS=" + ++seq + ",\"7870b07f\",\"6bb4d6ff\",\"c96e70cf\"" + eof;
+	  public static String configIDS(int seq){
+		  return "AT*CONFIG_IDS=" + seq + ",\"7870b07f\",\"6bb4d6ff\",\"c96e70cf\"";
 	  }
 
 	 
--- code/ardrone/src/PartieANOUARetANAS/Controller.java
+++ code/ardrone/src/PartieANOUARetANAS/Controller.java
@@ -8,11 +8,18 @@ package PartieANOUARetANAS;
 import java.io.*;
 import java.net.*;
 
-public class Controller extends Thread {
-	private Connection _connection;
+public class Controller {
+	private Connection _connection, _connectionFake;
 	
-	public Controller(String addr, int port, String eof){
-		_connection = new Connection(addr,port,eof);
+	public Controller(String addr, int port, String addrFake, int portFake){
+		_connection = new Connection(addr, port, "\r");
+		_connectionFake = new Connection(addrFake, portFake, "\n");
+	}
+	
+	public void sendMessage(String message)
+	{
+		_connection.sendMessage(message + "\r");
+		_connectionFake.sendMessage(message + "\n");
 	}
 	
 	public void run(){
@@ -22,46 +29,46 @@ public class Controller extends Thread {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		_connection.sendMessage(Commands.check(_connection.getSeq(),_connection.getEof()));
+		_connection.sendMessage(Commands.check(_connection.getSeq()));
 		
 	}
 	
 	//sequence d'initialisation a executer avant tout autre commande a envoyer
 	  public void initialize() throws InterruptedException{
-		  _connection.sendMessage(Commands.configIDS(_connection.getSeq(),_connection.getEof()));
-		  _connection.sendMessage(Commands.config("custom:session_id", "-all", _connection.getSeq(), _connection.getEof()));
+		  _connection.sendMessage(Commands.configIDS(_connection.getSeq()));
+		  _connection.sendMessage(Commands.config("custom:session_id", "-all", _connection.getSeq()));
 		  Thread.sleep(500);
 		  
-		  _connection.sendMessage(Commands.configIDS(_connection.getSeq(),_connection.getEof()));
-		  _connection.sendMessage(Commands.config("custom:profile_id", "-"+Convert.convertCRC32(Commands.PROFILE), _connection.getSeq(), _connection.getEof()));;
+		  _connection.sendMessage(Commands.configIDS(_connection.getSeq()));
+		  _connection.sendMessage(Commands.config("custom:profile_id", "-"+Convert.convertCRC32(Commands.PROFILE), _connection.getSeq()));;
 		  Thread.sleep(500);
 		  
-		  _connection.sendMessage(Commands.configIDS(_connection.getSeq(),_connection.getEof()));
-		  _connection.sendMessage(Commands.config("custom:application_id", "-"+Convert.convertCRC32(Commands.APPLI), _connection.getSeq(), _connection.getEof()));
+		  _connection.sendMessage(Commands.configIDS(_connection.getSeq()));
+		  _connection.sendMessage(Commands.config("custom:application_id", "-"+Convert.convertCRC32(Commands.APPLI), _connection.getSeq()));
 		  Thread.sleep(500);
 		  
-		  _connection.sendMessage(Commands.configIDS(_connection.getSeq(),_connection.getEof()));
-		  _connection.sendMessage(Commands.config("custom:session_id", Convert.convertCRC32(Commands.SESSION), _connection.getSeq(), _connection.getEof()));
+		  _connection.sendMessage(Commands.configIDS(_connection.getSeq()));
+		  _connection.sendMessage(Commands.config("custom:session_id", Convert.convertCRC32(Commands.SESSION), _connection.getSeq()));
 		  Thread.sleep(500);
 		  
-		  _connection.sendMessage(Commands.configIDS(_connection.getSeq(),_connection.getEof()));
-		  _connection.sendMessage(Commands.config("custom:application_id",Convert.convertCRC32(Commands.APPLI), _connection.getSeq(), _connection.getEof()));
+		  _connection.sendMessage(Commands.configIDS(_connection.getSeq()));
+		  _connection.sendMessage(Commands.config("custom:application_id",Convert.convertCRC32(Commands.APPLI), _connection.getSeq()));
 		  Thread.sleep(500);
 		  
-		  _connection.sendMessage(Commands.configIDS(_connection.getSeq(),_connection.getEof()));
-		  _connection.sendMessage(Commands.config("custom:profile_id", Convert.convertCRC32(Commands.PROFILE), _connection.getSeq(), _connection.getEof()));
+		  _connection.sendMessage(Commands.configIDS(_connection.getSeq()));
+		  _connection.sendMessage(Commands.config("custom:profile_id", Convert.convertCRC32(Commands.PROFILE), _connection.getSeq()));
 		  Thread.sleep(500);
 		  
-		  _connection.sendMessage(Commands.configIDS(_connection.getSeq(),_connection.getEof()));
-		  _connection.sendMessage(Commands.config("custom:application_desc", Commands.APPLI, _connection.getSeq(), _connection.getEof()));
+		  _connection.sendMessage(Commands.configIDS(_connection.getSeq()));
+		  _connection.sendMessage(Commands.config("custom:application_desc", Commands.APPLI, _connection.getSeq()));
 		  Thread.sleep(500);
 		  
-		  _connection.sendMessage(Commands.configIDS(_connection.getSeq(),_connection.getEof()));
-		  _connection.sendMessage(Commands.config("custom:profile_desc", Commands.PROFILE, _connection.getSeq(), _connection.getEof()));
+		  _connection.sendMessage(Commands.configIDS(_connection.getSeq()));
+		  _connection.sendMessage(Commands.config("custom:profile_desc", Commands.PROFILE, _connection.getSeq()));
 		  Thread.sleep(500);
 		  	
-		  _connection.sendMessage(Commands.configIDS(_connection.getSeq(),_connection.getEof()));
-		  _connection.sendMessage(Commands.config("custom:session_desc", Commands.SESSION, _connection.getSeq(), _connection.getEof()));
+		  _connection.sendMessage(Commands.configIDS(_connection.getSeq()));
+		  _connection.sendMessage(Commands.config("custom:session_desc", Commands.SESSION, _connection.getSeq()));
 		  Thread.sleep(500);
 	}
 }
