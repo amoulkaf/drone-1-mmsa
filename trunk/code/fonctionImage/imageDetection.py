f9ca95f82ab5df2b3099f756adc712ac8cc2333e
lec.geoffrey@gmail.com <lec.geoffrey@gmail.com@ef62bdd9-8e75-cf03-c011-c9a79ca1a455>
2014-11-05 12:41:40 +0000

    modif imdDetec

--- trunk/code/fonctionImage/imageDetection.py
+++ trunk/code/fonctionImage/imageDetection.py
@@ -1,7 +1,18 @@
 from PIL.Image import *
 
-def main():
-	img = open("../../doc/IHM/ihm.png")
-	Image.show(img)
+filePath = "../../doc/IHM/ihm.png"
 
-main()
+def detectPix(pix, img):
+	(l,h) = img.size
+	for x in range(l):
+		for y in range(h):
+			currentPix = img.getpixel((x,y))
+			if (currentPix == pix):
+				print("ok")
+
+def main():
+	img = open(filePath)
+	detectPix((255,0,255,255), img)
+
+if __name__ == "__main__": 
+	main()
