b77d32ef1f76b6303b652ce0f30281e1631c6dd0
lec.geoffrey <lec.geoffrey@gmail.com>
2014-11-05 14:52:17 +0000

    fonction image 90%

--- code/fonctionImage/imageDetection.py
+++ code/fonctionImage/imageDetection.py
@@ -1,18 +1,61 @@
+# this script finds in an image if there is a black/dark grey
+# area of 1/SCALE area
+
 from PIL.Image import *
 
-filePath = "../../doc/IHM/ihm.png"
+FILEPATH = "../../doc/IHM/ihm.png"
+# limit inf of r,g,b component
+RANGE_INF = 0
+# limit sup of r,g,b component
+RANGE_SUP = 50
+# scale of the area to find
+SCALE = 20
 
-def detectPix(pix, img):
+# is the current pixel black or dark grey 
+def pixelEqualsTo(pix):
+	r = pix[0]
+	g = pix[1]
+	b = pix[2]
+	for x in range(RANGE_INF, RANGE_SUP + 1) :
+		if (r,g,b) == (x,x,x):
+			return True
+
+# is there black pixels in area
+# area is 1/SCALE of img
+def isDetectedArea(img, x, y, currentPix):
+	(l,h) = img.size
+	l_area = l//SCALE
+	h_area = h//SCALE
+	isDifferent = False
+	if (x-l_area) > 0 and (y-h_area) > 0 and pixelEqualsTo(currentPix):
+		if (x+l_area) < l and (y+h_area) < h:
+			for i in range(x, x+l_area+1):
+				for j in range(y, y+h_area+1):
+					if img.getpixel((i,j)) != currentPix:
+						isDifferent = True
+						break
+			if not(isDifferent):
+				return True
+	return False
+
+# detect if there is an area of pixel 
+# [(RANGE_INF,RANGE_INF, RANGE_INF),(RANGE_SUP,RANGE_SUP,RANGE_SUP)]
+def detectPix(img):
 	(l,h) = img.size
 	for x in range(l):
 		for y in range(h):
 			currentPix = img.getpixel((x,y))
-			if (currentPix == pix):
-				print("ok")
+			#pixelEqualsTo(currentPix)
+			if isDetectedArea(img,x,y,currentPix):
+				print("Area detected")
+				print "color : ",currentPix
+				print "coordinates : ",(x,y)
+				return
+
 
 def main():
-	img = open(filePath)
-	detectPix((255,0,255,255), img)
+	img = open(FILEPATH)
+	detectPix(img)
 
 if __name__ == "__main__": 
 	main()
