fda743c99865cfc7016b64b7770f937413a3d577
lec.geoffrey <lec.geoffrey@gmail.com>
2014-11-05 15:31:42 +0000

    appel prog py dans prog java

---
+++ code/ardrone/src/Chrono.java
@@ -0,0 +1,35 @@
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+public class Chrono {
+		Calendar m_start = new GregorianCalendar();
+		Calendar m_stop = new GregorianCalendar();
+		
+		public Chrono() {		
+		}
+		
+		//Lance le chronomètre
+		public void start() {
+			m_start.setTime(new Date());
+		}
+		
+		//Arrète le chronomètre
+		public void stop() {
+			m_stop.setTime(new Date());
+		}
+		
+		//Retourne le nombre de millisecondes séparant l'appel des méthode start() et stop()
+		public long getMilliSec() {
+			return (m_stop.getTimeInMillis() - m_start.getTimeInMillis());
+		}
+		
+		//Affiche le nombre de millisecondes séparant l'appel des méthode start() et stop() sur la sortie standard
+		public void printMilliSec() {
+			if(getMilliSec() <= 0) {
+				System.out.println("Chronometer not stopped");
+			} else {
+				System.out.println("Exec time : " + getMilliSec() + " ms");
+			}
+		}
+	}
