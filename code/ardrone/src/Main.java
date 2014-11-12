b5311b7381cfce36d707c4dc451b44521ce3f468
lec.geoffrey <lec.geoffrey@gmail.com>
2014-11-12 14:28:49 +0000

    implementation algo python en java : 10 fois plus rapide a l'execution

--- code/ardrone/src/Main.java
+++ code/ardrone/src/Main.java
@@ -1,7 +1,10 @@
+import java.awt.image.BufferedImage;
 import java.io.*;
 import java.util.Date;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+
+import javax.imageio.ImageIO;
 
 import guiView.MainWindowGUI;
 
@@ -14,9 +17,93 @@ public class Main {
 		//Drone localhost = new Drone("localhost", 7000, "\n", "localhost");
 		  
 		testPy();
+		test();
 		
 		//ardrone.start();
 		//localhost.start();
+	}
+	
+	private static int[] getPixelData(BufferedImage img, int x, int y) {
+		int argb = img.getRGB(x, y);
+
+		int rgb[] = new int[] {
+		    (argb >> 16) & 0xff, //red
+		    (argb >>  8) & 0xff, //green
+		    (argb      ) & 0xff  //blue
+		};
+
+//		System.out.println("rgb: " + rgb[0] + " " + rgb[1] + " " + rgb[2]);
+		return rgb;
+	}
+
+	// Pire des cas (pas de detection de zone) : 0.185s
+	// Alors qu'en python : 1.7s
+	public static int test(){
+		BufferedImage img;
+		Chrono chrono = new Chrono();
+		chrono.start();
+
+	    try {
+	        img = ImageIO.read(new File("../../doc/IHM/ihm.png"));
+
+	        int[] rgb;
+	        int r,g,b;
+	        int r1,g1,b1;
+	        
+	        int width = img.getWidth();
+	        int height = img.getHeight();
+	        final int COLOR = 20;
+	        final int SCALE = 20;
+	        
+	        for(int x = 0; x < width; x++){
+	            for(int y = 0; y < height; y++){
+	            	rgb = getPixelData(img, x, y);
+	            	r = rgb[0];
+	            	g = rgb[1];
+	            	b = rgb[2];
+	            	
+	            	int l_area = width/SCALE;
+	            	int h_area = height/SCALE;
+	            	boolean dif = false;
+	            	if ((x+l_area) < width && 
+            			(y+h_area) < height && 
+            			r == 0 &&
+            			g == 0 &&
+            			b == 0){
+	            		for (int i=x; i<=x+l_area; i++){
+	            			for (int j=y; j<=y+h_area;j++){
+	            				//System.out.println(rgb);
+	            				int[] rgb1 = getPixelData(img, i, j);
+	            				r1 = rgb1[0];
+	        	            	g1 = rgb1[1];
+	        	            	b1 = rgb1[2];
+	            				//System.out.println("Pix : "+i+";"+j+"-"+rgb1[0]+" "+rgb1[1]+" "+rgb1[2]);
+	            				if (r1 != r || g1 != g || b1 != b){
+	            					dif = true;
+	            					//System.out.println(i+";"+j);
+	            					break;
+	            				}
+	            			}
+	            		}
+	            		
+	            		if (!dif){
+	            			System.out.println("Detected : "+x+";"+y);
+	            			chrono.stop();
+	            			chrono.printMilliSec();
+	            			return 0;
+	            		}
+	            	}
+	            }
+	        }
+	        System.out.println("Non detected");
+	        chrono.stop();
+			chrono.printMilliSec();
+
+
+	    } catch (IOException e) {
+	        e.printStackTrace();
+	    }
+	    return 0;
 	}
 	
 	public static void testPy(){
@@ -35,16 +122,18 @@ public class Main {
 				
 	            System.out.println("Here is the standard output of the command:");
 				while ((s = stdInput.readLine()) != null) {
-	                if (s.equals("true")){
-	                	System.out.println("Zone detectee");
+					System.out.println(s);
+					if (s.equals("true")){
+	                	p.destroy();
+	                	break;
 	                }
+	              
 	            }
 				
-				System.out.println("Here is the standard error of the command:");
-	            while ((s = stdError.readLine()) != null) {
-	                System.out.println(s);
-	            }
-	            chrono.stop();
+	           // while ((s = stdError.readLine()) != null) {
+	            //    System.out.println(s);
+	           // }
+				chrono.stop();
 				chrono.printMilliSec();
 				
 			} catch (IOException e) {
